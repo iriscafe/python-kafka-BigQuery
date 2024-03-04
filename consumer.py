67a0b81ab1721db8b66519820d9e0714a7834bcd
Iris Cafe <irislisboa.c@gmail.com>
2024-03-03 22:59:18 -0300

    Initial commit

---
+++ consumer.py
@@ -0,0 +1,28 @@
+from confluent_kafka import Consumer, KafkaError
+
+conf = {
+    'bootstrap.servers': 'localhost:9092',  # Endereço e porta do broker Kafka
+    'group.id': 'group_id',                 # Identificador do grupo de consumidores
+    'auto.offset.reset': 'earliest'          # Configuração de onde começar a consumir
+}
+
+# Cria um consumidor Kafka
+consumer = Consumer(conf)
+
+consumer.subscribe(['data_sense'])
+
+while True:
+    msg = consumer.poll(timeout=1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        if msg.error().code() == KafkaError._PARTITION_EOF:
+            continue
+        else:
+            print(msg.error())
+            break
+
+    print('Mensagem recebida: {}'.format(msg.value().decode('utf-8')))
+
+consumer.close()
